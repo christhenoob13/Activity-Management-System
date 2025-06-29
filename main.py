d2a7a94f3428b36fcb84efa3c6e9560247dff7a6
Christopher <greegmon13@gmail.com>
2025-06-29 12:07:27 +0800

    hshshhs

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from auth import auth
 from views import view
 
 app = Flask(__name__)
-app.secret_key = "&U-7Bjueh&:£77-jisiwiiJiebz8eh3iU*y682I"
+app.secret_key = "&U-7Bjueh&:£77-ji[ SIKRETONG MALUPET ]z8eh3iU*y682I"
 
 app.register_blueprint(auth, url_prefix='/auth')
 app.register_blueprint(view)
 
 if __name__ == '__main__':
-  app.run(debug=False)
+  app.run(debug=True)
--- views.py
+++ views.py
@@ -8,8 +8,40 @@ from flask import (
 
 view = Blueprint('view',__name__)
 
+# subjects
+data = [
+  {
+    "subject": "Philosophy",
+    "teacher": "Mary Grace Remulla"
+  },
+  {
+    "subject": "Entrepreneurship",
+    "teacher": "Cristina Mendoza"
+  },
+  {
+    "subject": "Contemporary Philippine Arts",
+    "teacher": "Renz Cruz"
+  },
+  {
+    "subject": "Understanding Culture, Society and Politics",
+    "teacher": "Nelson U. Rubio"
+  },
+  {
+    "subject": "21st Century",
+    "teacher": "Catherine Bergado"
+  },
+  {
+    "subject": "Physical Education ans Health",
+    "teacher": "Edison V. Palceso"
+  },
+  {
+    "subject": "Practical Research 2",
+    "teacher": "Jaime R. Soriano"
+  }
+]
+
 @view.route('/')
 def home():
   if not session.get('is_login'):
     return redirect(url_for('auth.login'))
-  return render_template('home.html')
+  return render_template('home.html', subjects=data)
